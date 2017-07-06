a775c0fcf2e16457cb98395a115eefd10a23968d
Roberto Hernández Tamayo <hetr@praxis.com.mx>
2017-07-06 17:10:52 -0500

    Feature - InspectFragment - Vista y actividad
    -Se agregó una actividad que se encargara de gestionar el llamado al servicio de Flickr y los eventos de gesto.
    -Se modificó el layout y se implementaron las funciones necesarias para iniciar la actividad de búsqueda.

---
+++ app/src/main/java/com/professional/micromaster/photolibrary/fragments/inspect/SearchActivity.java
@@ -0,0 +1,18 @@
+package com.professional.micromaster.photolibrary.fragments.inspect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.professional.micromaster.photolibrary.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        String tags = getIntent().getStringExtra(InspectFragment.KEY_TAGS);
+        Log.d("TAG",tags);
+    }
+}
